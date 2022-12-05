e9d5628a279355a6bffb9a57b833d9c6bafbfc27
katChyb <64010913+katChyb@users.noreply.github.com>
2022-12-05 18:43:58 +0100

    added basketPopupTest from part 2

--- SeleniumAdvancedPart2/src/test/java/basekt/BasketTest.java
+++ SeleniumAdvancedPart2/src/test/java/basekt/BasketTest.java
@@ -1,6 +1,7 @@
 package basekt;
 
 import base.Pages;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -10,7 +11,8 @@ import pages.cart.CartPopup;
 public class BasketTest extends Pages {
     private static Logger log = LoggerFactory.getLogger(BasketTest.class);
 
-    @Test
+
+    @RepeatedTest(10)
     @Tag("Basket Popup Test")
     public void basketPopupTest() {
         topMenuPage.clickInArtCategory();
